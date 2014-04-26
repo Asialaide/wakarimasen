cc77b5e964c93b123db7b849f29c51eb52f7c801
Chris <stuntaneous@gmail.com>
2014-04-26 23:14:19 +1000

    Fresh.
    Nothing going on. Added SFML libraries.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace wakarimasen
 {
@@ -10,6 +10,9 @@ namespace wakarimasen
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Woot");
+
         }
     }
 }
